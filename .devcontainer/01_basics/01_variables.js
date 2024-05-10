20d444af8e7c628b63b0691636c076b0e39e9357
Gaurav Arya <144447573+gauravaryaarya@users.noreply.github.com>
2024-05-10 20:02:49 +0530

    01_Variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,31 @@
+//documentation - MDN
+"use strict"; // treat all JS code as newer version
+
+const accountId = 817702722 //constant value
+let accountEmail = "Gauravarya70422" //scope
+var accountPassword = "1234" //global
+accountCity= "Delhi" //global
+let accountState
+
+console.log(accountState); //undefined
+
+console.log(accountId);
+
+//multiple printing
+
+console.table([
+    accountEmail,
+    accountId,
+    accountPassword,
+    accountCity
+])
+
+
+//alert("invalid") // browser method not nodeJS
+
+
+
+
+ 
+
+
