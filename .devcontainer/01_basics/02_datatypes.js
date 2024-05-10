d85517492886e2f1b53ab40e68ac5b049866dc30
Gaurav Arya <144447573+gauravaryaarya@users.noreply.github.com>
2024-05-10 20:03:03 +0530

    02_datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+let name="Gaurav" //String
+let age=20 //Number
+let ishandsome= true //Bool
+
+//number, bigint, string, boolean
+//null(Stand Alone Value), undefined 
+//symbol(Unique in React), objects
+
+//typeof
+
+console.log(typeof "Gaurav");
+console.log(typeof age);
+console.log(typeof ishandsome);
+console.log(typeof null) 
+
+
+
+
