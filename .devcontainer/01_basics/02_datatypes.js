ae487d12701ebe526ee9822a10bbb944121a77bb
Gaurav Arya <144447573+gauravaryaarya@users.noreply.github.com>
2024-05-10 23:07:37 +0530

    new leanings day-2

--- .devcontainer/01_basics/02_datatypes.js
+++ .devcontainer/01_basics/02_datatypes.js
@@ -2,9 +2,34 @@ let name="Gaurav" //String
 let age=20 //Number
 let ishandsome= true //Bool
 
+//primitive(value) 7 types
 //number, bigint, string, boolean
 //null(Stand Alone Value), undefined 
-//symbol(Unique in React), objects
+//symbol(Unique in React)
+
+//non-primitive(refrence)
+//array. objects, functions
+
+const heros=["shaktiman","naagraj","doga"] //array
+
+//object
+let myobj={
+    name: "Gaurav",
+    age:20,
+
+}
+
+//function
+const myfunction = function(){
+    console.log("helloow");
+}
+
+
+
+
+
+//type-safety
+
 
 //typeof
 
@@ -12,6 +37,13 @@ console.log(typeof "Gaurav");
 console.log(typeof age);
 console.log(typeof ishandsome);
 console.log(typeof null) 
+
+//symbol
+const id = Symbol('123')
+const anotherid = Symbol('123')
+//even value same but not equal ===
+
+
 
 
 
---
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -0,0 +1,34 @@
+let score= "69a"
+console.log(typeof score) 
+
+// string to number 
+
+let value= Number(score)
+console.log(value) //NaN 
+console.log(typeof score) 
+console.log(typeof value) 
+
+//Operations
+
+let val = 3
+let negval = -val
+let to_power = 3**2
+
+let str1= "Gaurav"
+let str2 = " Arya"
+let str3 = str1+str2 //Gaurav Arya
+
+let A = "1" + 2 //12
+let B = 1 + "2" //12
+let C = "1" + 2 + 2 //122
+let D = 1 + 2 + "2" //32
+//printint true gives true o/p
+//printing +true gives 1 as o/p
+// +"" o/p= 0
+ 
+
+
+
+
+
+
